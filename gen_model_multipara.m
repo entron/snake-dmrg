f33ea83120647949954c270895017880c5401f28
Cheng Guo <9cheng.guo@gmail.com>
2012-11-28 23:33:18 +0100

    Minor changes.

--- gen_model_multipara.m
+++ gen_model_multipara.m
@@ -1,8 +1,8 @@
 Omega=0.05;
-for Lambda=1.1
-    for BathL=800
+for Lambda=2
+    for BathL=50
         for z=1
-            for omegaratio=0.02:0.02:2
+            for omegaratio=1:2
             gen_model(Omega,omegaratio,Lambda,BathL,z);
             end
         end
